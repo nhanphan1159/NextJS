c227157f44a3227ba9e9905cd10b78d89e353209
nhan phan <nhanphan1159@gmail.com>
2024-09-19 23:13:34 +0700

    back route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+const privatePaths = ['/me']
+const authPaths = ['/login', '/register']
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
